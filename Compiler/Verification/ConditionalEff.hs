f87c4692de1935fbbae7b6a255b2a1425369f56b
songyahui <31238748+songyahui@users.noreply.github.com>
2019-09-18 16:30:56 +0800

    a

--- Compiler/Verification/ConditionalEff.hs
+++ Compiler/Verification/ConditionalEff.hs
@@ -79,12 +79,30 @@ splitDot eff =
         OR e1 e2 ->  (splitDot e1)++ (splitDot e2) 
         Neg e -> [(eff, Empty)]
 
+checkRedundent :: Condition -> Condition
+checkRedundent con = 
+    let helper con1 con2 = 
+            case con2 of 
+                AndCon p1 p2 -> 
+                    if con1 == p1 || con1 == p2 then True
+                    else (helper con1 p1) && (helper con1 p2) 
+                other -> if con1 == other then True 
+                         else False
+    in case con of 
+        AndCon s1 s2 -> 
+            if s1 == s2 then s1
+            else if helper s1 s2 == True then checkRedundent s2
+            else AndCon s1 (checkRedundent s2)
+        otherwise -> con
+        
+             
+
 normalCon :: Condition -> Condition
 normalCon con =
     case con of 
         AndCon a b ->
             case (a, b) of 
-                (FALSE, _) -> TRUE
+                (FALSE, _) -> FALSE
                 (_, FALSE) -> FALSE
                 (TRUE, a) -> normalCon a
                 (a, TRUE) -> normalCon a
@@ -95,15 +113,15 @@ normalCon con =
                         else (Gt s1 n2)
                 (Gt s1 n1, Lt s2 n2) -> 
                     if s1 /= s2 then con 
-                    else if n1 > n2 then FALSE
+                    else if n1 >= n2 then FALSE
                          else con 
                 (Gt s1 n1, Eq s2 n2) -> 
                     if s1 /= s2 then con 
-                    else if n1 > n2 then FALSE
+                    else if n1 >= n2 then FALSE
                          else (Eq s2 n2) 
                 (Lt s1 n1, Gt s2 n2) -> 
                     if s1 /= s2 then con 
-                    else if n2 > n1 then FALSE
+                    else if n2 >= n1 then FALSE
                          else con
                 (Lt s1 n1, Lt s2 n2) -> 
                     if s1 /= s2 then con 
@@ -111,34 +129,28 @@ normalCon con =
                          else (Lt s1 n2) 
                 (Lt s1 n1, Eq s2 n2) -> 
                     if s1 /= s2 then con 
-                    else if n2 > n1 then FALSE
+                    else if n2 >= n1 then FALSE
                          else (Eq s2 n2) 
                 (Eq s1 n1, Gt s2 n2) -> 
                     if s1 /= s2 then con 
-                    else if n2 > n1 then FALSE
+                    else if n2 >= n1 then FALSE
                          else (Eq s1 n1)
                 (Eq s1 n1, Lt s2 n2) -> 
                     if s1 /= s2 then con 
-                    else if n2 < n1 then FALSE
+                    else if n2 <= n1 then FALSE
                          else (Eq s1 n1)
                 (Eq s1 n1, Eq s2 n2) -> 
                     if s1 /= s2 then con 
                     else if n2 /= n1 then FALSE
                          else (Eq s1 n1)
-                otherwise -> normalCon (AndCon (normalCon a ) (normalCon b))
+                (a, b) ->  
+                    let na = normalCon a
+                        nb = normalCon b
+                    in if na == FALSE || nb == FALSE || na == TRUE || nb == TRUE 
+                        then normalCon (checkRedundent$  AndCon na nb)
+                       else (checkRedundent$ AndCon na nb)
         otherwise -> con
 
-
-t_t con evn (h1, rest1) (h2,rest2) = 
-    case (h1, h2) of 
-        (Ttimes e1 sv1, Ttimes e2 sv2) ->
-            let (tree, result) = (containment e1 e2 [])
-            in 
-                if result ==  False then (tree, result)
-                else 
-                    let afterL = rest1
-                        afterS = normal ((Dot (normal (Ttimes e2 (Minus sv2 sv1 ))) rest2))
-                    in entail (con, normal afterL) (con, normal afterS) evn
 
 
 checkFirstElm :: Condition -> Effect -> Effect -> Env -> (Tree String ,Bool)
@@ -149,7 +161,15 @@ checkFirstElm con eff1 eff2 evn =
         checkHead (h1, rest1) (h2,rest2) =
             case (h1, h2) of
                 (Ttimes e1 sv1, Ttimes e2 sv2) -> 
-                    t_t con evn (h1, rest1) (h2,rest2)
+                    let (tree, result) = (containment e1 e2 [])
+                    in 
+                        if result ==  False then (tree, result)
+                        else 
+                            let afterL = rest1
+                                afterS = normal ((Dot (normal (Ttimes e2 (Minus sv2 sv1 ))) rest2))
+                            in entail (con, normal afterL) (con, normal afterS) evn
+                
+                            
                 (Ttimes e1 sv1, Star e2) ->
                     let (tree, result) = (containment e1 e2 [])
                     in 
@@ -204,7 +224,7 @@ entail :: ConditionalEff -> ConditionalEff -> Env -> (Tree String ,Bool)
 entail ceff1 ceff2 evn = 
     let (con1, eff1) = ceff1
         (con2, eff2) = ceff2
-        andCondition = normalCon (AndCon con1 con2)
+        andCondition = checkRedundent $ normalCon (AndCon con1 con2)
     in 
         if andCondition == FALSE 
             then (Node ((printEntailCondEff ceff1 ceff2 )++ " [Condition Contradictory]") [] ,False)
@@ -212,6 +232,9 @@ entail ceff1 ceff2 evn =
             checkFirstElm andCondition (normal eff1) (normal eff2) evn
 
 
+{-
+extentCondition -> used to capture the branching of a effect if it contains any Ttimes part. 
+-}
 extentCondition :: ConditionalEff -> [ConditionalEff]
 extentCondition (condition, effect) =
     let helper (frame, con) eff = 
@@ -219,37 +242,54 @@ extentCondition (condition, effect) =
                 Dot eff1 eff2 ->  
                     let combine = [(x,y) | x <- helper (frame, con) eff1, y <- helper (frame, con) eff2] 
                         merge ((conh ,effh),(conr, effr)) =
-                            (AndCon conh conr, normal (Dot frame (normal (Dot effh effr))))
+                            (checkRedundent $ normalCon (AndCon  (AndCon con conh) (AndCon con conr)), normal (Dot frame (normal (Dot effh effr))))
                     in map merge combine 
                 OR eff1 eff2 ->  
                     let combine = [(x,y) | x <- helper (frame, con) eff1, y <- helper (frame, con) eff2] 
                         merge ((conh ,effh),(conr, effr)) =
-                            (AndCon conh conr, normal (Dot frame (normal (OR effh effr))))
+                            (checkRedundent $ normalCon (AndCon  (AndCon con conh) (AndCon con conr)), normal (Dot frame (normal (OR effh effr))))
                     in map merge combine 
-                Ttimes ins (Iden s) -> [(Eq s 0, Empty) , (Gt s 0, Ttimes ins (Iden s))]
-
+                Ttimes ins (Iden s) -> [(Eq s 0, Empty) , (Gt s 0, eff)]
+                Ttimes ins (Minus (Iden s) (Value n)) -> [(Eq s n, Empty) , (Gt s n, eff)]
                 otherwise -> [(con, normal (Dot frame eff))]
-    in helper (Empty , condition)  effect
+        filterNotFalse (condition, effect) = 
+            case condition of
+                FALSE -> False
+                otherwise -> True
+    in filter filterNotFalse (helper (Empty , condition)  effect)
 
 p_Con_R :: ConditionalEff -> ConditionalEff  -> IO ()
 p_Con_R r s  = 
-    let (tree , res) = entail r s []
-    in
-     if res then 
-        do {
-        putStrLn ("============= Report =============");
-        putStrLn ("GOAL: " ++ printEntailCondEff r s);
-        print "Succeed!" ;
-        putStrLn $ drawTree tree
-        }
+    let 
+        conRL = extentCondition r
+        conSL = extentCondition s
+        combine = [(x,y) | x <- conRL, y <- conSL] 
+        
+        printReprot [] = print ("END.") 
+        printReprot (l:ls)=  
+            let (r, s) = l
+                (tree , res) = entail r s []
+            in 
+                if res then 
+                    do {
+                    putStrLn ("============= Report =============");
+                    putStrLn ("GOAL: " ++ printEntailCondEff r s);
+                    print "Succeed!" ;
+                    putStrLn $ drawTree tree;
+                    printReprot ls
+                    }
+                 else 
+                    do{
+                    putStrLn ("============= Report =============");
+                    putStrLn ("GOAL: " ++ printEntailCondEff r s);
+                    print "fail";
+                    putStrLn $ drawTree tree;
+                    printReprot ls
+                    }
+
+
+    in  printReprot combine
      
-     else 
-        do {
-        putStrLn ("============= Report =============");
-        putStrLn ("GOAL: " ++ printEntailCondEff r s);
-        print "fail";
-        putStrLn $ drawTree tree
-        }
         {-
         case getResidue r s [] of 
             Nothing -> 
