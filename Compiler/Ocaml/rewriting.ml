8e82eb7023f5f76ff0544501fe05900f0e645161
songyahui <31238748+songyahui@users.noreply.github.com>
2019-10-17 13:40:09 +0800

    m

--- Compiler/Ocaml/rewriting.ml
+++ Compiler/Ocaml/rewriting.ml
@@ -1,4 +1,21 @@
 open String
+open Z3
+open Z3.Symbol
+open Z3.Sort
+open Z3.Expr
+open Z3.Boolean
+open Z3.FuncDecl
+open Z3.Goal
+open Z3.Tactic
+open Z3.Tactic.ApplyResult
+open Z3.Probe
+open Z3.Solver
+open Z3.Arithmetic
+open Z3.Arithmetic.Integer
+open Z3.Arithmetic.Real
+open Z3.BitVector
+open List
+
 
 (*----------------------------------------------------
 ---------------------DATA STRUCTURE-----------------
@@ -35,7 +52,7 @@ type context = Delta of effect list
 ----------------------PRINTING------------------------
 ----------------------------------------------------*)
 
-let rec showTerms t = 
+let rec showTerms (t:terms) = 
   match t with
     Var name -> name
   | Plus (t, num) -> (showTerms t) ^ ("+") ^ (string_of_int num)
@@ -79,10 +96,125 @@ let rec showContext d =
   ;;
 
 
+
 (*----------------------------------------------------
 ----------------------CONTAINMENT--------------------
 ----------------------------------------------------*)
 exception Foo of string
+
+let rec getTermNameFromTerm (t:terms) =
+  match t with 
+    Var name -> name
+  | Plus (ter, num) -> getTermNameFromTerm ter
+  | Minus (ter, num) -> getTermNameFromTerm ter
+  ;;
+
+let rec getZ3ExprFromTerm (term:terms) ctx = 
+  match term with
+    Var name -> 
+      let name_is = (Symbol.mk_string ctx name) in
+      let x_is = Integer.mk_const ctx name_is in
+      x_is
+  | Plus (t, num) -> 
+      let name_is = (Symbol.mk_string ctx (getTermNameFromTerm t))  in
+      let x_is = Integer.mk_const ctx name_is in
+      let make_plus = Arithmetic.mk_add ctx [ x_is ; (Integer.mk_numeral_i ctx num)] in
+      make_plus
+  | Minus (t, num) -> 
+      let name_is = (Symbol.mk_string ctx (getTermNameFromTerm t))  in
+      let x_is = Integer.mk_const ctx name_is in
+      let make_minus = Arithmetic.mk_add ctx [ x_is ; (Integer.mk_numeral_i ctx (0 - num))] in
+      make_minus
+  ;;
+
+let rec getZ3ConstrainFromPure pi ctx acc= 
+  match pi with 
+    TRUE -> acc 
+  | FALSE -> 
+      let name_is = (Symbol.mk_string ctx "constructFalse") in
+      let x_is = Integer.mk_const ctx name_is in
+      let x1 = (mk_gt ctx (x_is) 
+          (Integer.mk_numeral_i ctx 0)) in
+      let x2 = (mk_eq ctx (x_is) 
+          (Integer.mk_numeral_i ctx 0)) in
+      append acc [x1;x2]
+  | Gt (term, num) -> 
+      let expr = getZ3ExprFromTerm term ctx in 
+      let assertion = mk_gt ctx expr (Integer.mk_numeral_i ctx num) in
+      append acc [assertion]
+  | Lt (term, num) -> 
+      let expr = getZ3ExprFromTerm term ctx in 
+      let assertion = (mk_lt ctx expr (Integer.mk_numeral_i ctx num)) in
+      append acc [assertion]
+  | Eq (term, num) -> 
+      let expr = getZ3ExprFromTerm term ctx in 
+      let assertion = (mk_eq ctx expr (Integer.mk_numeral_i ctx num)) in
+      append acc [assertion]
+  | PureAnd (pi1,pi2) -> 
+      let assert1 = getZ3ConstrainFromPure pi1 ctx [] in
+      let assert2 = getZ3ConstrainFromPure pi2 ctx [] in
+      append (append acc assert1) assert2
+  | Neg piN -> 
+      let assert1 = getZ3ConstrainFromPure piN ctx [] in
+      let makenot = mk_not ctx (List.nth assert1 0)  in
+      append acc [makenot]
+  | _ ->raise ( Foo "getZ3ConstrainFromPure exception!")
+;;
+
+let askZ3 pi = 
+  let context = Z3.mk_context [] in
+  let constrains = getZ3ConstrainFromPure pi context [] in
+  let solver = Z3.Solver.mk_solver context None in
+  let () = Z3.Solver.add solver constrains in
+    match Z3.Solver.check solver [] with
+    | UNSATISFIABLE -> 
+      (*Printf.printf "unsat\n" *)
+      false
+    | UNKNOWN -> 
+      (*Printf.printf "unknown"*)
+      false
+    | SATISFIABLE -> true
+        (*match Z3.Solver.get_model solver with
+        | None -> ()
+        | Some model ->
+            Printf.printf "%s\n"
+                (Z3.Model.to_string model)*)
+
+let rec normalES es = 
+  match es with
+    Bot -> es
+  | Emp -> es
+  | Event ev -> es
+  | Cons (es1, es2) -> 
+      (match (normalES es1, es2) with 
+        (Emp, _) -> es2
+      | (Bot, _) -> Bot
+      | (normal_es1, _) -> Cons (normal_es1, normalES es2)
+      ;)
+  | ESOr (es1, es2) -> 
+      (match (normalES es1, normalES es2) with 
+        (Bot, Bot) -> Bot
+      | (Bot, norml_es2) -> norml_es2
+      | (norml_es1, Bot) -> norml_es1
+      | (norml_es1, norml_es2) -> ESOr (norml_es1, norml_es2)
+      ;)
+  | Ttimes (es1, terms) -> normalES es1
+  | Omega es1 -> normalES es1
+  ;;
+
+let rec normalEffect eff =
+  match eff with
+    Effect (p, es) -> 
+      if (askZ3 p) == false then Effect (FALSE,  Bot)
+      else if normalES es == Bot then Effect (FALSE,  Bot)
+      else Effect (p , normalES es)
+  | Disj (eff1, eff2) -> 
+      match (normalEffect eff1, normalEffect eff2) with
+        (Effect (FALSE,  Bot), _) -> normalEffect eff2
+      | (_, Effect (FALSE,  Bot)) -> normalEffect eff1
+      | _ -> Disj (normalEffect eff1, normalEffect eff2)
+  ;;
+
 
 let rec nullable eff =
   match eff with
@@ -94,7 +226,7 @@ let rec nullable eff =
         | Event ev -> false 
         | Cons (es1 , es2) -> nullable (Effect (p,es1)) && nullable (Effect (p,es2))
         | ESOr (es1 , es2) -> nullable (Effect (p,es1)) || nullable (Effect (p,es2))
-        | Ttimes (es1, t) -> true (*TODO: need to call the Z3*)
+        | Ttimes (es1, t) -> askZ3 (PureAnd (p, Eq (t,0))) (*TODO: need to call the Z3*)
         | Omega es1 -> false
       )
   | Disj (_, _) -> raise ( Foo "nullable exception!")
@@ -111,45 +243,49 @@ let rec fst es =
 
 let appendEff_ES eff es = 
   match eff with 
-    Effect (p , es_eff) ->  (p, Cons (es_eff, es))
+    Effect (p , es_eff) ->  Effect(p, Cons (es_eff, es))
   | _ -> raise ( Foo "appendEff_ES exception!")
   ;;
 
 let rec derivative eff ev =
   match eff with
-  Effect (p , es) -> 
+    Effect (p , es) -> 
       (
         match es with
           Bot -> Effect (FALSE,  Bot)
         | Emp -> Effect (FALSE,  Bot)
         | Event ev1 -> 
-            if  ev1 == ev then Effect (p, Emp) else Effect (p, Bot)
-        (*
+            if (String.compare ev1 ev) == 0 then Effect (p, Emp) else Effect (p, Bot)
         | Omega es1 -> appendEff_ES (derivative (Effect (p, es1)) ev) es
-        | ESOr (es1 , es2) -> (p, ESOr (derivative es1 ev, derivative es2 ev))
+        | ESOr (es1 , es2) -> Disj (derivative (Effect (p,es1)) ev, derivative (Effect (p,es2)) ev)
         | Ttimes (es1, t) -> 
-            let pi = PureAnd (Gt (t, 0), p) in
-            let efF = derivative es1 ev in 
-            let eff_2 = Ttimes (es1, Minus (t, 1)) in
-             (pi, efF)
-        (*TODO: need to call the Z3*)
-        
-        (*Cons (es1 , es2) -> (p, Cons (derivative es1 ev, derivative es2 ev))
-        *)*)
-        
+            let pi = PureAnd (Gt (t, 1), p) in
+            let efF = derivative (Effect (pi, es1)) ev in 
+            let esT_minus1 = Ttimes (es1, Minus (t, 1)) in
+            appendEff_ES efF esT_minus1
+        | Cons (es1 , es2) -> 
+            if nullable (Effect (p,es1)) == true 
+            then let efF = derivative (Effect (p, es1)) ev in 
+                 let effL = appendEff_ES efF es2 in 
+                 let effR = derivative (Effect (p, es2)) ev in 
+                 Disj (effL, effR)
+            else let efF = derivative (Effect (p, es1)) ev in 
+                 appendEff_ES efF es2    
       )
-  | Disj (_, _) -> raise ( Foo "derivative exception!")
+  | Disj (eff1, eff2) -> Disj (derivative eff1 ev, derivative eff2 ev)
   ;;
 
 (*----------------------------------------------------
 ----------------------TESTING-------------------------
 ----------------------------------------------------*)
 
-let ttest = (Minus ((Var "song"),1));;
-let estest = Omega (Event "a");;
-let puretest = Gt (ttest, 0) ;;
+let ttest = (Plus ((Var "song"),1));;
+let ttest1 = (Var "t");;
+let estest = Cons (Ttimes ((Event "a"), Var "t"),  (Event "a"));;
+let puretest =  Eq (ttest1, 0);;
 let testes = Effect (puretest, estest);; 
 let testcontext = Delta [testes; testes];;
+let testD = derivative testes ( "a");;
 
 (*Printf.printf "%s" (showTerms  ttest);;
 Printf.printf "%s" (showES estest);;
@@ -158,27 +294,8 @@ Printf.printf "%s" (showPure puretest);;
 
 Printf.printf "%s" (showEffect testes);;
 Printf.printf "%s" (showContext testcontext );;*)
-Printf.printf "%s" ("\n")
+let a = askZ3 puretest ;;
 
-let context = Z3.mk_context []
-let solver = Z3.Solver.mk_solver context None
+Printf.printf "%s" (showEffect (normalEffect testD));;
 
-let xsy = Z3.Symbol.mk_string context "x"
-let x = Z3.Boolean.mk_const context xsy
-
-let () = Z3.Solver.add solver [x]
-
-let main () =
-    match Z3.Solver.check solver [] with
-    | UNSATISFIABLE -> Printf.printf "unsat\n"
-    | UNKNOWN -> Printf.printf "unknown"
-    | SATISFIABLE ->
-        match Z3.Solver.get_model solver with
-        | None -> ()
-        | Some model ->
-            Printf.printf "%s\n"
-                (Z3.Model.to_string model)
-
-
-let a = main ()
-
+Printf.printf "%s" ("\n");;
